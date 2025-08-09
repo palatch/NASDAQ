06e54c7d903367141a51153c5c6ea03449036f37
palatch <36508488+palatch@users.noreply.github.com>
2025-08-09 11:25:38 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', (e)=>{
-  e.waitUntil(caches.open('dm-bk-cache-v2').then(cache=>cache.addAll([
-    './','./index.html','./app.js','./sw.js','./manifest.webmanifest',
-    './icon-192.png','./icon-512.png'
-  ])));
-});
-self.addEventListener('fetch', (event)=>{
-  event.respondWith(caches.match(event.request).then(resp=> resp || fetch(event.request)));
-});
