3dcd57941021fea6485c8a727d275ef17d8e6a4e
palatch <36508488+palatch@users.noreply.github.com>
2025-08-10 17:13:21 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener('install',(e)=>{
-  e.waitUntil(caches.open('dm-fullsite-split-fullsite-split-v6-2025-08-10').then(cache=>cache.addAll([
-    './','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'
-  ])));
-});
-self.addEventListener('fetch',(event)=>{
-  event.respondWith(caches.match(event.request).then(resp=>resp||fetch(event.request)));
-});
