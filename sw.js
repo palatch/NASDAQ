0d043693030f332635c8df9aca0bd2326888396a
palatch <36508488+palatch@users.noreply.github.com>
2025-08-10 22:41:10 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,4 @@
-// Placeholder for service worker
-self.addEventListener('install', event => {
-  console.log('Service Worker installing');
+self.addEventListener('install', e => {
+  self.skipWaiting();
 });
-self.addEventListener('fetch', event => {
-  // Cache logic can be added here
-});
+self.addEventListener('fetch', e => {});
