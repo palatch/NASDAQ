99a5983eb88607662d535a677d9df2f3f8797ce4
palatch <36508488+palatch@users.noreply.github.com>
2025-08-10 18:04:23 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,1 @@
-self.addEventListener('install',(e)=>{
-  e.waitUntil(caches.open('dm-fullsite-split-fullsite-split-v6-2025-08-10').then(cache=>cache.addAll([
-    './','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'
-  ])));
-});
-self.addEventListener('fetch',(event)=>{
-  event.respondWith(caches.match(event.request).then(resp=>resp||fetch(event.request)));
-});
+self.addEventListener('install',e=>{e.waitUntil(caches.open('dl-v8').then(c=>c.addAll(['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon-192.png','./icon-512.png'])))});self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
